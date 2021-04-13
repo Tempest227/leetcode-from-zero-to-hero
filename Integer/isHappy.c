41825510b73a2366d8c25f014f31b7cceaed690b
tempest227 <2629649128@qq.com>
2021-04-13 15:51:04 +0800

    202. 快乐数

--- Integer/isHappy.c
+++ Integer/isHappy.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 int next(int x)
 {
     int t = 0;
@@ -36,43 +35,4 @@ bool isHappy(int n){
     }
 
     return false;
-=======
-int next(int x)
-{
-    int t = 0;
-    int sum = 0;
-
-    while(x!=0)
-    {
-        t = x % 10;
-        sum += t * t;
-        x /= 10;
-    }
-
-    return sum;
-}
-
-bool isHappy(int n){
-    if (n == 1)
-    {
-        return true;
-    }
-
-    int fast = next(n);    
-    int slow = n;
-
-    while(slow != fast)
-    {
-        fast = next(fast);
-        fast = next(fast);
-        slow = next(slow);
-
-        if(fast == 1)
-        {
-            return true;
-        }
-    }
-
-    return false;
->>>>>>> 7458b2de946ad6e5055e56cac4900d4e4b695ac5
 }
