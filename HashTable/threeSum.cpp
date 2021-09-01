ad9a91c52c3cd2790a0f7abbc3721704158df78a
tempest227 <2629649128@qq.com>
2021-09-01 21:29:04 +0800

    三数之和

---
+++ HashTable/threeSum.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        if (nums.size() == 0)
+        {
+            return {};
+        }
+
+        if (nums[0] > 0)
+        {
+            return {};
+        }
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++)
+        {
+            //去重
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            while(left < right)
+            {
+                if (nums[i] + nums[left] + nums[right] > 0)
+                {
+                    right--;
+                }
+                else if (nums[i] + nums[left] + nums[right] < 0)
+                {
+                    left++;
+                }
+                else
+                {
+                    result.push_back(vector<int>{nums[i], nums[left], nums[right]});
+                    //去重
+                    while(left < right && nums[right] == nums[right - 1])right--;
+                    while(left < right && nums[left] == nums[left + 1])left++;
+
+                    left++;
+                    right--;
+                }
+            }
+           
+        }
+         return result;
+    }
+};
